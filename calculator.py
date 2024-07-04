951d5cd59d31483223df99b367c5a73b0cade515
Flup3ers <flup3ers@gmail.com>
2024-07-04 14:00:57 +0200

    Add CLI interface for basic operations

--- calculator.py
+++ calculator.py
@@ -12,3 +12,30 @@ def division(a, b):
         return a / b
     else:
         return "Error Division by zero"
+    
+def main():
+    print("Hello ~Calculator!")
+    print("Chose option:")
+    print("1. Addition")
+    print("2. Substraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Insert Option (1/2/3/4): ")
+
+    num1 = float(input("Insert first number: "))
+    num2 = float(input("Insert second number: "))
+
+    if choice == '1':
+        print("Result:", addition(num1, num2))
+    elif choice == '2':
+        print("Result:", substraction(num1, num2))
+    elif choice == '3':
+        print("Result:", multiplication(num1, num2))
+    elif choice == '4':
+        print("Result:", division(num1, num2))
+    else:
+        print("Incorect option")
+
+if __name__ == "__main__":
+    main()
